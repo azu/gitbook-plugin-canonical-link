ceb6a17dee8989173b1c8a288ee329c1f8e3ff52
Johan Preynat <johan.preynat@gmail.com>
2016-04-25 16:40:56 +0200

    Remove index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// LICENSE : MIT
-"use strict";
-module.exports = {
-    website: {
-        html: {
-            "head:end": '<link rel="canonical" href="' + this.options.canonicalURL + '">',
-        }
-    }
-};
